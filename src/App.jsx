d5c24b9c217a742169e8bad62658e19e31263237
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-02-05 09:39:44 +0330

    delete old code

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,10 @@ import "./App.css";
 function App() {
     const [count, setCount] = useState(0);
 
-    /*  const users = [
-        {
-            firstName: "Gholamreza",
-            lastName: "Hassanzadeh",
-        },
-    ];
-
-    const element = <h1>{users[0].firstName}</h1>; */
-    /* const user = "gholamreza"; */
-    const users = [
-        {
-            id: 1,
-            firstName: "Reza",
-        },
-        {
-            id: 2,
-            firstName: "Mammd",
-        },
-        {
-            id: 3,
-            firstName: "Hassan",
-        },
-    ];
+   
     return (
         <div className="App">
             <div>
-                {/* {user === "gholamreza" ? <p>true</p> : <p>false</p>} */}
                 <a href="https://vitejs.dev" target="_blank">
                     <img src="/vite.svg" className="logo" alt="Vite logo" />
                 </a>
@@ -43,12 +20,6 @@ function App() {
                     />
                 </a>
             </div>
-            {users.map((user) => (
-                <p style={{ display: "flex" }} key={user.id}>
-                    {user.firstName}
-                </p>
-            ))}
-
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
