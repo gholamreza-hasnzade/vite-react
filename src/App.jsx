65b09df382fc07831f277629e87f8bb011a72303
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-02-05 13:32:37 +0330

    create counter applaction

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ import "./App.css";
 function App() {
     const [count, setCount] = useState(0);
 
+    const increaseCount = () => {
+        setCount(count + 1);
+    };
+    const decreaseCount = () => {
+        setCount(count - 1);
+    };
+
+    const reastCount = () => {
+        setCount(0);
+    };
     return (
         <div className="App">
-            <Counter count={count} />
-            <button onClick={() => setCount((count) => count + 1)}>
-                add count
-            </button>
-            <button onClick={() => setCount((count) => count - 1)}>
-                mines count
-            </button>
+            <Counter
+                count={count}
+                inc={increaseCount}
+                des={decreaseCount}
+                reast={reastCount}
+            />
         </div>
     );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Counter = ({ count }) => {
+const Counter = ({ count, inc, des, reast }) => {
     return (
-        <div>
+        <>
             <h1>شمارنده : {count}</h1>
-        </div>
+            <div className="d-flex m-x-1">
+                <button onClick={inc}>+</button>
+                <button onClick={des}>-</button>
+                <button onClick={reast}>0</button>
+            </div>
+        </>
     );
 };
 Counter.propTypes = {
     count: PropTypes.number,
-}; 
+};
 
 export default Counter;
