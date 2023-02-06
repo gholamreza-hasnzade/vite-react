c7b1f80de960e1a4aeb0e34f6750d08dbfc30a7f
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-02-06 15:37:07 +0330

    fixed loading

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             <Navbar />
-            <Contacts contacts={getContacts} />
+            <Contacts contacts={getContacts}  loading={loading}/>
         </div>
     );
 }
