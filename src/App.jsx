d0258aed633776b410f79a922681a6dfacbdf534
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-02-07 11:27:24 +0330

    add route contacts and Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Contacts, Navbar } from "./components";
-
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 function App() {
     const [getContacts, setContacts] = useState([]);
@@ -9,7 +9,15 @@ function App() {
     return (
         <div className="App">
             <Navbar />
-            <Contacts contacts={getContacts}  loading={loading}/>
+            <Routes>
+                <Route path="/" element={<Navigate to={"/contacts"} />} />
+                <Route
+                    path="/contacts"
+                    element={
+                        <Contacts contacts={getContacts} loading={loading} />
+                    }
+                />
+            </Routes>
         </div>
     );
 }
