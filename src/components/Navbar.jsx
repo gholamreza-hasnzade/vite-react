27224626c40df40fffb168dc244ab9de12757ac4
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-02-12 09:22:08 +0330

    show search bar in contacts url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,36 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { BACKGROUND, PURPLE } from "../helpers/colors";
 import SearchContact from "./Contacts/SearchContact";
 
 const Navbar = ({ query, search }) => {
-  return (
-    <nav
-      className="navbar navbar-dark navbar-expand-sm shadow-lg"
-      style={{ backgroundColor: BACKGROUND }}
-    >
-      <div className="container">
-        <div className="row w-100">
-          <div className="col">
-            <div className="navbar-brand">
-              <i className="fas fa-id-badge" style={{ color: PURPLE }} /> وب
-              اپلیکیشن مدیریت{"  "}
-              <span style={{ color: PURPLE }}>مخاطبین</span>
+    const location = useLocation();
+    return (
+        <nav
+            className="navbar navbar-dark navbar-expand-sm shadow-lg"
+            style={{ backgroundColor: BACKGROUND }}
+        >
+            <div className="container">
+                <div className="row w-100">
+                    <div className="col">
+                        <div className="navbar-brand">
+                            <i
+                                className="fas fa-id-badge"
+                                style={{ color: PURPLE }}
+                            />{" "}
+                            وب اپلیکیشن مدیریت{"  "}
+                            <span style={{ color: PURPLE }}>مخاطبین</span>
+                        </div>
+                    </div>
+                    {location.pathname === "/contacts" ? (
+                        <div className="col">
+                            <SearchContact query={query} search={search} />
+                        </div>
+                    ) : null}
+                </div>
             </div>
-          </div>
-          <div className="col">
-            <SearchContact query={query} search={search} />
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 };
 
 export default Navbar;
